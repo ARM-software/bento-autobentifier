c969cd5cb18a17f8123f3180be9bde4e51db82e5
Michael Bartling <michael.bartling@arm.com>
2020-06-01 16:49:18 -0500

    Get global variable references in function parsing

--- autobentifier/module_analyzer.py
+++ autobentifier/module_analyzer.py
@@ -1,98 +1,129 @@
 from collections import defaultdict
 from llvmlite import binding
+import logging
 import json
+import re
 import sys
 
 ## Normally would use LLVM to get this info, but that's a real PITA right now. Opt for simple json parser instead
-#binding.initialize()
-#binding.initialize_all_targets()
-#binding.initialize_all_asmprinters()
+binding.initialize()
+binding.initialize_all_targets()
+binding.initialize_all_asmprinters()
 #with open("demo.elf.ll") as fp:
-#    x = fp.read()
+#  x = fp.read()
 #xl = binding.parse_assembly(x)
 
+logger = logging.getLogger("ModuleAnalyzer")
+
 class FunctionParam:
-    def __init__(self, jsonStr):
-        self.json = jsonStr
+  def __init__(self, jsonStr):
+    self.json = jsonStr
 
-    @property
-    def name(self):
-        return self.json["name"]
+  @property
+  def name(self):
+    return self.json["name"]
 
-    @property
-    def realName(self):
-        return self.json["realName"]
+  @property
+  def realName(self):
+    return self.json["realName"]
 
-    @property
-    def type(self):
-        return self.json["type"]["llvmIr"]
+  @property
+  def type(self):
+    return self.json["type"]["llvmIr"]
 
 
 class Function:
-    def __init__(self, jsonStr):
-        self.json = jsonStr
+  def __init__(self, jsonStr):
+    self.json = jsonStr
+    self.global_references = set()
 
-    @property
-    def startAddr(self):
-        return int(self.json["startAddr"], 16)
+  @property
+  def startAddr(self):
+    return int(self.json["startAddr"], 16)
 
-    @property
-    def endAddr(self):
-        return int(self.json["endAddr"], 16)
+  @property
+  def endAddr(self):
+    return int(self.json["endAddr"], 16)
 
-    @property
-    def name(self):
-        return self.json["name"]
-    
-    def size(self):
-        return self.endAddr - self.startAddr
+  @property
+  def name(self):
+    return self.json["name"]
+  
+  def size(self):
+    return self.endAddr - self.startAddr
 
-    @property
-    def returnType(self):
-        return self.json["returnType"]["llvmIr"]
-    
-    def parameters(self):
-        return [FunctionParam(x) for x in self.json["parameters"]]
+  @property
+  def returnType(self):
+    return self.json["returnType"]["llvmIr"]
+  
+  def parameters(self):
+    return [FunctionParam(x) for x in self.json["parameters"]]
+
+  def add_global_reference(self, global_name):
+    self.global_references.add(global_name)
 
 
 class GlobalVar:
-    def __init__(self, jsonStr):
-        self.json = jsonStr
+  def __init__(self, jsonStr):
+    self.json = jsonStr
 
-    @property
-    def name(self):
-        return self.json["name"]
+  @property
+  def name(self):
+    return self.json["name"]
 
-    @property
-    def realName(self):
-        return self.json["realName"]
+  @property
+  def realName(self):
+    return self.json["realName"]
 
-    @property
-    def type(self):
-        return self.json["type"]["llvmIr"]
+  @property
+  def type(self):
+    return self.json["type"]["llvmIr"]
 
-    @property
-    def storage(self):
-        return self.json["storage"]
+  @property
+  def storage(self):
+    return self.json["storage"]
 
 class ModuleAnalyzer:
-    def __init__(self):
-        self.functions = defaultdict(dict)
-        self.globals = defaultdict(dict)
+  def __init__(self):
+    self.functions = defaultdict(dict)
+    self.globals = defaultdict(dict)
 
-    def analyze(self, fname):
-        with open(fname) as fp:
-            js = json.load(fp)
-            for function in js["functions"]:
-                fname = function["name"]
-                self.functions[fname] = Function(function)
-            for g in js["globals"]:
-                gname = g["name"]
-                self.globals = GlobalVar(g)
+  def analyze(self, json_fname, ll_fname):
+    with open(json_fname) as fp:
+      js = json.load(fp)
+      for g in js["globals"]:
+        gname = g["name"]
+        self.globals[gname] = GlobalVar(g)
+      for function in js["functions"]:
+        fname = function["name"]
+        self.functions[fname] = Function(function)
+
+    with open(ll_fname) as fp:
+      x = fp.read()
+      xl = binding.parse_assembly(x)
+      for fName in self.functions:
+        try:
+          f = xl.get_function(fName)
+        except NameError:
+          logger.info("Function %s not found in llvmlite" % fName)
+          continue
+        except:
+          logger.warning("Unhandled exception occured in llvmlite.get_function")
+          continue
+
+        for b in f.blocks:
+          # getting Globals is a real PITA, it's easier to parse the opcode
+          for i in b.instructions:
+            op_code = str(i)
+            matches = re.findall(r"@(?P<global_ref>[_a-zA-Z]\w*)", op_code)
+            if matches:
+              for match in matches:
+                if match in self.globals:
+                  self.functions[fName].add_global_reference(match)
 
 
 if __name__ == '__main__':
-    m = ModuleAnalyzer
-    m.analyze(sys.argv[1])
+  m = ModuleAnalyzer
+  m.analyze(sys.argv[1])
 
 
